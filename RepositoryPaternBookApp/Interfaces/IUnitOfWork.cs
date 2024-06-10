7293cedc9d83058fd3d20187352e3a966c9bbbe3
VelSal <v.salvatore@live.be>
2024-06-10 13:17:41 +0200

    IUnit of Work and UnitOfWork

---
+++ RepositoryPaternBookApp/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using RepositoryPaternBookApp.Models.DomainModels;
+
+namespace RepositoryPaternBookApp.Interfaces
+{
+	public interface IUnitOfWork : IDisposable
+	{
+        IRepository<Book> Books { get; }
+		IRepository<Author> Authors { get; }
+		IRepository<Genre> Genres { get; }
+		IRepository<BookGenre> BooksGenres { get; }
+
+		Task<int> CompleteAsync();
+    }
+}
---
+++ RepositoryPaternBookApp/Repositories/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using RepositoryPaternBookApp.Interfaces;
+using RepositoryPaternBookApp.Models.DomainModels;
+
+namespace RepositoryPaternBookApp.Repositories
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		public IRepository<Book> Books => throw new NotImplementedException();
+
+		public IRepository<Author> Authors => throw new NotImplementedException();
+
+		public IRepository<Genre> Genres => throw new NotImplementedException();
+
+		public IRepository<BookGenre> BooksGenres => throw new NotImplementedException();
+
+		public Task<int> CompleteAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
