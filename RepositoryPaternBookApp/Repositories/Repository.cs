9ee04291e38d5978d5888d52047576f28ec28ebb
VelSal <v.salvatore@live.be>
2024-06-10 11:30:55 +0200

    Repositories folder + Repository class

---
+++ RepositoryPaternBookApp/Repositories/Repository.cs
@@ -0,0 +1,32 @@
+﻿using RepositoryPaternBookApp.Interfaces;
+
+namespace RepositoryPaternBookApp.Repositories
+{
+	public class Repository<T> : IRepository<T> where T : class
+	{
+		public Task<T> AddAsync(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> DeleteAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<T> GetAllAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> GetByIdAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> UpdateAsync(T entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
