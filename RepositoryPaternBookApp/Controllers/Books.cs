3cec51399baef4296fc9f98cd49187c6271a8173
VelSal <v.salvatore@live.be>
2024-06-11 10:15:03 +0200

    Controller logic

--- RepositoryPaternBookApp/Controllers/Books.cs
+++ RepositoryPaternBookApp/Controllers/Books.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RepositoryPaternBookApp.Interfaces;
+using RepositoryPaternBookApp.Models.DomainModels;
+using RepositoryPaternBookApp.Models.ViewModels;
+using System.Reflection;
+
 
 namespace RepositoryPaternBookApp.Controllers
 {
@@ -8,10 +12,90 @@ namespace RepositoryPaternBookApp.Controllers
 		private readonly IUnitOfWork _unitOfWork;
 		private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public Books(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+		public Books(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
 		{
 			_unitOfWork = unitOfWork;
 			_webHostEnvironment = webHostEnvironment;
 		}
-    }
+
+		public async Task<IActionResult> Create()
+		{
+			var viewModel = new CreateBookViewModel
+			{
+				Authors = (await _unitOfWork.Authors.GetAllAsync()).ToList(),
+				Genres = (await _unitOfWork.Genres.GetAllAsync()).ToList()
+			};
+			return View(viewModel);
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(CreateBookViewModel viewModel)
+		{
+			//Si la data entrée n'est pas valide il faut montrer qqch sinon crash
+			if (!ModelState.IsValid)
+			{
+				await InvalidData(viewModel);
+			}
+			string imagePath = await SaveImageAsync(viewModel.Image);
+
+			if (imagePath == null)
+			{
+				await InvalidData(viewModel);
+				return View(viewModel);
+			}
+
+			var newBook = new Book
+			{
+				Title = viewModel.Book.Title,
+				AuthorId = viewModel.SelectedAuthorId,
+				IsAvailable = viewModel.Book.IsAvailable,
+				IsBestSeller = viewModel.Book.IsBestSeller,
+				IsNewRelease = viewModel.Book.IsNewRelease,
+				BindingType = viewModel.Book.BindingType,
+				ImagePath = imagePath,
+			};
+			await _unitOfWork.Books.AddAsync(newBook);
+			await _unitOfWork.CompleteAsync();
+
+			if (viewModel.SelectedGenres != null && viewModel.SelectedGenres.Any())
+			{
+				foreach (var genreId in viewModel.SelectedGenres)
+				{
+					var bookGenre = new BookGenre
+					{
+						BookId = newBook.BookId,
+						GenreId = genreId,
+					};
+					await _unitOfWork.BooksGenres.AddAsync(bookGenre);
+				}
+				await _unitOfWork.CompleteAsync();
+			}
+			return RedirectToAction(nameof(Index));
+		}
+
+		private async Task InvalidData(CreateBookViewModel viewModel)
+		{
+			viewModel.Authors = (await _unitOfWork.Authors.GetAllAsync()).ToList();
+			viewModel.Genres = (await _unitOfWork.Genres.GetAllAsync()).ToList();
+		}
+		private async Task<string> SaveImageAsync(IFormFile image)
+		{
+			if (image == null || image.Length == 0)
+			{
+				return null;
+			}
+
+			string uploadFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+			string uniqueFileName = Guid.NewGuid().ToString() + "_" + image.FileName;
+			string filePath = Path.Combine(uploadFolder, uniqueFileName);
+
+			using (var fileStream = new FileStream(filePath, FileMode.Create))
+			{
+				await image.CopyToAsync(fileStream);
+			}
+
+			return "/images/" + uniqueFileName;
+		}
+	}
 }
+
---
+++ RepositoryPaternBookApp/Models/ViewModels/CreateBookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using RepositoryPaternBookApp.Models.DomainModels;
+
+namespace RepositoryPaternBookApp.Models.ViewModels
+{
+	public class CreateBookViewModel
+	{
+        public Book Book { get; set; }
+        public int SelectedAuthorId { get; set; }
+        public List<int> SelectedGenres { get; set; } = new List<int>();
+        public List<Author> Authors { get; set; } = new List<Author>();
+        public List<Genre> Genres { get; set; } = new List<Genre>();
+        public IFormFile Image { get; set; }
+        public string? ImagePath { get; set; } = "/images/Default.png";
+    }
+}
